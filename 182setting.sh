6e839e25e3926f2a1f8a87f132f2252ff7cd9f8a
Ray <heguorui0402@gmail.com>
2022-11-29 10:03:55 +0800

    docs: updata script

--- 182setting.sh
+++ 182setting.sh
@@ -1,22 +1,20 @@
-sudo apt update
-sudo apt upgrade
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt --fix-broken install -y
 
 sudo usermod -aG sudo matlab
 sudo passwd matlab
 
+
+
 sudo apt-get -y install labguage-pack-zh-han*
-sudo apt-get install language-pack-zh-han*
-sudo apt install $(check-language-support)
-sudo apt install bash-completion
+sudo apt-get -y install language-pack-zh-han*
+sudo apt -y install $(check-language-support)
+sudo apt -y install bash-completion
 sudo apt-get -y install wget
 
 cd ~
 source .bashrc 
-mkdir Downloads
-cd Downloads/
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
 
 sudo apt-get -y install nano
 sudo apt -y install net-tools
---
+++ install.sh
@@ -0,0 +1,6 @@
+cd ~
+cd Downloads/
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget -c https://az764295.vo.msecnd.net/stable/6261075646f055b99068d3688932416f2346dd3b/code_1.73.1-1667967334_amd64.deb
+sudo dpkg -i code_1.73.1-1667967334_amd64.deb
