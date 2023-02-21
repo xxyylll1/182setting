cf42d9f0196ad1217167e1d214c101190573aef4
YungRui <75406355+GuoRui0402@users.noreply.github.com>
2023-02-21 12:49:47 +0800

    Update 182setting.sh

--- 182setting.sh
+++ 182setting.sh
@@ -5,23 +5,20 @@ sudo apt --fix-broken install -y
 sudo usermod -aG sudo matlab
 sudo passwd matlab
 
-
+sudo apt-get -y install ssh
+sudo apt-get -y install openssh-server
+sudo nano /etc/ssh/sshd_config
+sudo /etc/init.d/ssh restart
+sudo apt --fix-broken install -y
 
 sudo apt-get -y install labguage-pack-zh-han*
 sudo apt-get -y install language-pack-zh-han*
 sudo apt -y install $(check-language-support)
 sudo apt -y install bash-completion
 sudo apt-get -y install wget
+sudo apt-get -y install nano
+sudo apt -y install net-tools
+sudo apt --fix-broken install -y
 
 cd ~
 source .bashrc 
-
-sudo apt-get -y install nano
-sudo apt -y install net-tools
-sudo apt-get -y install ssh
-sudo apt-get -y install openssh-server
-
-sudo nano /etc/ssh/sshd_config
-sudo /etc/init.d/ssh restart
-ssh-keygen
-sudo apt --fix-broken install -y
