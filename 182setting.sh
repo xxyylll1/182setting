c51847c96b96611a202b8894d461033cb228b919
YungRui <75406355+GuoRui0402@users.noreply.github.com>
2023-08-25 17:05:35 +0800

    Update 182setting.sh

--- 182setting.sh
+++ 182setting.sh
@@ -2,25 +2,23 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt --fix-broken install -y
 
+sudo apt-get -y install net-tools
+sudo apt-get -y install wget
+sudo apt-get -y install nano
+sudo apt-get -y install language-pack-zh-han*
+sudo apt-get -y install $(check-language-support)
+sudo apt-get -y install bash-completion
+sudo apt-get -y install p7zip-full p7zip-rar
+sudo apt-get -y install xdg-utils
+sudo apt-get -y install ssh
+sudo apt-get -y install openssh-server
+sudo apt --fix-broken install -y
+
 sudo usermod -aG sudo matlab
 sudo passwd matlab
 
-sudo apt-get -y install ssh
-sudo apt-get -y install openssh-server
 sudo nano /etc/ssh/sshd_config
 sudo /etc/init.d/ssh restart
-sudo apt --fix-broken install -y
-
-sudo apt-get -y install labguage-pack-zh-han*
-sudo apt-get -y install language-pack-zh-han*
-sudo apt -y install $(check-language-support)
-sudo apt -y install bash-completion
-sudo apt-get -y install wget
-sudo apt-get -y install nano
-sudo apt -y install net-tools
-sudo apt install p7zip-full p7zip-rar
-sudo apt install xdg-utils
-sudo apt --fix-broken install -y
 
 cd ~
 source .bashrc 
